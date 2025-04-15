c0059f544038b9ae7c44d2e97db7c793cb8f0656
Burhan Akkus <burgcanmail@gmail.com>
2025-04-16 01:54:55 +0200

    Week 4 svm derivative

--- Week4 Regression and Gradiend Descent/main.py
+++ Week4 Regression and Gradiend Descent/main.py
@@ -169,3 +169,59 @@ print(ans)
 # Test case 2
 ans = svm_obj(x_1, y_1, th1, th1_0, 0.0)
 print(ans)
+
+
+################################################
+
+# Returns the gradient of hinge(v) with respect to v.
+def d_hinge(v):
+    return np.where(v < 1, -1, 0)
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th
+def d_hinge_loss_th(x, y, th, th0):
+    return d_hinge(y * (np.dot(np.transpose(th),x) + th0)) * (y * x)
+
+
+# Returns the gradient of hinge_loss(x, y, th, th0) with respect to th0
+def d_hinge_loss_th0(x, y, th, th0):
+    return d_hinge(y * (np.dot(np.transpose(th),x) + th0)) * y
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th
+def d_svm_obj_th(x, y, th, th0, lam):
+    average = np.vstack(np.average(d_hinge_loss_th(x,y,th,th0),axis=1))
+    regularizor = lam * 2 * th
+    return average + regularizor
+
+# Returns the gradient of svm_obj(x, y, th, th0) with respect to th0
+def d_svm_obj_th0(x, y, th, th0, lam):
+    average = np.average(d_hinge_loss_th0(x,y,th,th0))
+    return np.array([average]).reshape(-1, 1)
+
+# Returns the full gradient as a single vector (which includes both th, th0)
+def svm_obj_grad(X, y, th, th0, lam):
+    return np.vstack([d_svm_obj_th(X,y,th,th0,lam),d_svm_obj_th0(X,y,th,th0,lam)])
+
+X1 = np.array([[1, 2, 3, 9, 10]])
+y1 = np.array([[1, 1, 1, -1, -1]])
+th1, th10 = np.array([[-0.31202807]]), np.array([[1.834     ]])
+X2 = np.array([[2, 3, 9, 12],
+               [5, 2, 6, 5]])
+y2 = np.array([[1, -1, 1, -1]])
+th2, th20=np.array([[ -3.,  15.]]).T, np.array([[ 2.]])
+
+print(d_hinge(np.array([[ 71.]])).tolist())
+print(d_hinge(np.array([[ -23.]])).tolist())
+print(d_hinge(np.array([[ 71, -23.]])).tolist())
+
+print(d_hinge_loss_th(X2[:,0:1], y2[:,0:1], th2, th20).tolist())
+print(d_hinge_loss_th(X2, y2, th2, th20).tolist())
+print(d_hinge_loss_th0(X2[:,0:1], y2[:,0:1], th2, th20).tolist())
+print(d_hinge_loss_th0(X2, y2, th2, th20).tolist())
+
+print(d_svm_obj_th(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
+print(d_svm_obj_th(X2, y2, th2, th20, 0.01).tolist())
+print(d_svm_obj_th0(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
+print(d_svm_obj_th0(X2, y2, th2, th20, 0.01).tolist())
+
+print(svm_obj_grad(X2, y2, th2, th20, 0.01).tolist())
+print(svm_obj_grad(X2[:,0:1], y2[:,0:1], th2, th20, 0.01).tolist())
